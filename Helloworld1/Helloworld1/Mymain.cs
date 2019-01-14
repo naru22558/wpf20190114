af822b2a8c41a34fbcce94e2085016167c75de7a
fitnesskimybkm <fitnesskim@naver.com>
2019-01-14 11:17:36 +0900

    WPF HelloWorld

---
+++ Helloworld1/Helloworld1/Mymain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Helloworld1
+{
+    class Mymain
+    {
+        public static void Main()
+        {
+            Application app = new Application();
+
+            app.Run(new MainWindow());
+
+        }
+
+    }
+}
